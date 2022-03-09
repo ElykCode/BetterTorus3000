25a757b4fe6d193b23a0aaf11900eb7fb61f7719
Kyle S <6378468+ElykCode@users.noreply.github.com>
2022-03-08 21:39:47 -0600

    added calculateTorus function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iosteam>
+#include <iostream>
+
+float calculateTorus(float minorRadius, float majorRadius)
+{
+  // this program will use 3.14 for pi, as it doesn't need to be super acccurate
+  return ( 3.14 * ( minorRadius * minorRadius ) ) * ( 2 * 3.14 * majorRadius );
+}
 
 int main()
 {
+  std::cout << "Torus volume is: " << calculateTorus(3, 4) << '\n';
   return 0;
 }
