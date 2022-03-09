cb1983f8e70ae07843a67e163d79dfb441dc5e1d
Kyle S <6378468+ElykCode@users.noreply.github.com>
2022-03-09 08:54:52 -0600

    added a volume variable for readability

--- main.cpp
+++ main.cpp
@@ -16,11 +16,15 @@ int main()
   int majorRadiusInput { };
   std::cin >> majorRadiusInput;
 
+  // this is another variable only used once, but it's much more readable than
+  // calling the function in the print statement
+  float volume{ calculateTorus(minorRadiusInput, majorRadiusInput) };
+
   // The major radius of a torus is larger than the minor radius, so check that!
   if (minorRadiusInput < majorRadiusInput)
   {
     std::cout << "Your calculated torus volume is: "
-    << calculateTorus(minorRadiusInput, majorRadiusInput)
+    << volume
     << '\n';
   }
   else
